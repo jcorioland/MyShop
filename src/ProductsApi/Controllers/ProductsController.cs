370f213833810e2ecc6cf6551dbb68be7fea5bb3
Julien Corioland <julien.corioland@live.fr>
2016-10-01 14:11:23 +0200

    update products Api Version Experiences

--- src/ProductsApi/Controllers/ProductsController.cs
+++ src/ProductsApi/Controllers/ProductsController.cs
@@ -13,7 +13,7 @@ namespace ProductsApi.Controllers
         [HttpGet]
         public string Get()
         {
-            string productsApiVersion = "1.0.0";
+            string productsApiVersion = "1.2.0";
             return $"Exécution de ProductsApi en version {productsApiVersion} - Serveur : {Environment.MachineName}";
         }
     }
