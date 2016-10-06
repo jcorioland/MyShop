4e63dd833eebd4c1201550dfdc15df3ee701104f
Julien Corioland <julien.corioland@live.fr>
2016-10-06 14:50:30 +0200

    update context path in build ps script

--- src/build-images.ps1
+++ src/build-images.ps1
@@ -1,19 +1,19 @@
 param([string]$dockerLogin,[string]$dockerPassword)
 
 echo "Build dotnetcore image"
-docker build -t jcorioland/dotnetcore:win2k16tp5 DotnetDocker
+docker build -t jcorioland/dotnetcore:win2k16tp5 src\DotnetDocker
 
 echo "Build products-api image"
-docker build -t jcorioland/shop-products-api:win2k16tp5 ProductsApi
+docker build -t jcorioland/shop-products-api:win2k16tp5 src\ProductsApi
 
 echo "Build ratings-api image"
-docker build -t jcorioland/shop-ratings-api:win2k16tp5 RatingsApi
+docker build -t jcorioland/shop-ratings-api:win2k16tp5 src\RatingsApi
 
 echo "Build recommandations-api image"
-docker build -t jcorioland/shop-recommandations-api:win2k16tp5 RecommandationsApi
+docker build -t jcorioland/shop-recommandations-api:win2k16tp5 src\RecommandationsApi
 
 echo "Build front image"
-docker build -t jcorioland/shop-front:win2k16tp5 ShopFront
+docker build -t jcorioland/shop-front:win2k16tp5 src\ShopFront
 
 echo "Login to Docker Hub"
 docker login -u $dockerLogin -p $dockerPassword
