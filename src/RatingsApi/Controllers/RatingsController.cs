b0d03941d4ddeb3491a134e26767ffbeb63222c0
Julien Corioland <julien.corioland@live.fr>
2016-11-16 20:23:07 +0100

    update API versions

--- src/RatingsApi/Controllers/RatingsController.cs
+++ src/RatingsApi/Controllers/RatingsController.cs
@@ -13,7 +13,7 @@ namespace RatingsApi.Controllers
         [HttpGet]
         public string Get()
         {
-            string ratingsApiVersion = "1.0";
+            string ratingsApiVersion = "1.2";
             return $"Executing RatingsApi version {ratingsApiVersion}. Hostname : {Environment.MachineName}";
         }
     }
